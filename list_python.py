cd4ed2b422ec5f16c62088b29e3c218bc03985a7
Jitendrakum.Nighot <jitendrakumar.nighot@riotinto.com>
2021-08-05 14:35:12 +0530

    python file updated

--- list_python.py
+++ list_python.py
@@ -6,3 +6,10 @@ print("The Reverse List is", List1)
 List2 = [91, 67, 120, 34, 76, 54, 78, 87, 56, 64, 345]
 List2.sort()
 print("Ths sorted list is", List2)
+
+List3 = []
+List3 = List1.copy()
+print("List3 =", List3)
+
+indexvalue = List2[2:6]
+print("The Index value are", indexvalue)
