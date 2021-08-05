cd044a1e19fac045a468545818e169233ea7d5e6
Jitendrakum.Nighot <jitendrakumar.nighot@riotinto.com>
2021-08-05 14:30:58 +0530

    python file

---
+++ list_python.py
@@ -0,0 +1,8 @@
+import os
+List1 = [8, 9, 3, 6, 1, 10]
+List1.reverse()
+print("The Reverse List is", List1)
+
+List2 = [91, 67, 120, 34, 76, 54, 78, 87, 56, 64, 345]
+List2.sort()
+print("Ths sorted list is", List2)
